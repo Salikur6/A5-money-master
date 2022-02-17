753c628c22eb3f99554cfbd70a2b12a624b5b347
salikur6 <salikur6@gmail.com>
2022-02-17 14:15:37 +0600

    fix saving amount calculation

--- js/script.js
+++ js/script.js
@@ -11,6 +11,12 @@ function moneyInner(amountId) {
     return moneyText;
 }
 
+function moneyInputValue(inputId) {
+    const inputText = document.getElementById(inputId).value;
+    const inputValue = parseInt(inputText)
+
+    return inputValue;
+}
 
 function subtraction(amountText, first, secound) {
     const balancetext = document.getElementById(amountText);
@@ -19,27 +25,41 @@ function subtraction(amountText, first, secound) {
 
 //  adding total expenses values-----------------------------------
 function getExpenses() {
-    const foodValueText = document.getElementById('food').value;
-    const foodValue = parseInt(foodValueText);
+    const foodValue = moneyInputValue('food');
+    const rentValue = moneyInputValue('rent');
+    const clothesValue = moneyInputValue('clothes');
 
-
-    const rentValueText = document.getElementById('rent').value;
-    const rentValue = parseInt(rentValueText);
-
-
-    const clothesValueText = document.getElementById('clothes').value;
-    const clothesValue = parseInt(clothesValueText);
 
     const totalExpenses = foodValue + rentValue + clothesValue;
     return totalExpenses;
 }
 
+// function mainBalance() {
+//     subtraction('balance', getIncome(), getExpenses());
+
+// }
 document.getElementById('calculate').addEventListener('click', function() {
 
-    const amountText = document.getElementById('expenses')
+    // const amountText = document.getElementById('expenses')
+    const amountText = moneyInner('expenses')
     amountText.innerText = getExpenses();
 
     subtraction('balance', getIncome(), getExpenses());
 
 
 })
+
+// saving calculation part ---------------------------------------------
+
+document.getElementById('save').addEventListener('click', function() {
+
+    const saveInput = document.getElementById('save-input').value;
+    const saveValue = parseInt(saveInput);
+
+
+    const savingAmount = moneyInner('saving-amount')
+
+    const savingAmountdivided = (saveValue / 100) * getIncome();
+
+    savingAmount.innerText = savingAmountdivided;
+});
